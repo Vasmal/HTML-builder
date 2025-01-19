fcd6a4dea3a1d8b7bb08f8a27a70e411162d6d28
Vasilii Malai <malaivasili@gmail.com>
2025-01-19 16:08:49 +0300

    feat: complete '03-files-in-folder' task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const name = file.name;
+        const filePath = path.join(secretFolderPath, name);
+        const ext = path.extname(filePath);
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          const size = stats.size;
+          console.log(
+            `${name.replace(ext, '')} - ${ext.replace('.', '')} - ${size}`,
+          );
+        });
+      }
+    });
+  }
+});
