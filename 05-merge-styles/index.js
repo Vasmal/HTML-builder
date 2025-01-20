ea4015a8b032cfebb0b9c6a41ee989077c617852
Vasilii Malai <malaivasili@gmail.com>
2025-01-21 02:10:52 +0300

    fix: incorrect order of created style file at task 5 (fixed)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,15 +7,14 @@ const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
 async function pipe() {
   await fs.promises.writeFile(bundle, '');
 
-  const output = fs.createWriteStream(bundle);
   const files = await fs.promises.readdir(styleFiles, { withFileTypes: true });
   for (let file of files) {
     if (file.isFile()) {
       const filePath = path.join(styleFiles, file.name);
       const ext = path.extname(filePath);
       if (ext === '.css') {
-        const input = fs.createReadStream(filePath);
-        input.pipe(output);
+        const input = await fs.promises.readFile(filePath, 'utf-8');
+        await fs.promises.appendFile(bundle, input);
       }
     }
   }
